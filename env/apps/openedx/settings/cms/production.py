b9a17513c25819c931ce95ed9824e1a65fd9253e
slymnGms <suleyman_gumus@outlook.com.tr>
2021-08-31 08:13:11 +0000

    init

---
+++ env/apps/openedx/settings/cms/production.py
@@ -0,0 +1,195 @@
+# -*- coding: utf-8 -*-
+import os
+from cms.envs.production import *
+
+####### Settings common to LMS and CMS
+import json
+import os
+
+from xmodule.modulestore.modulestore_settings import update_module_store_settings
+
+# Mongodb connection parameters: simply modify `mongodb_parameters` to affect all connections to MongoDb.
+mongodb_parameters = {
+    "host": "mongodb",
+    "port": 27017,
+    
+    "user": None,
+    "password": None,
+    
+    "db": "openedx",
+}
+DOC_STORE_CONFIG = mongodb_parameters
+CONTENTSTORE = {
+    "ENGINE": "xmodule.contentstore.mongo.MongoContentStore",
+    "ADDITIONAL_OPTIONS": {},
+    "DOC_STORE_CONFIG": DOC_STORE_CONFIG
+}
+# Load module store settings from config files
+update_module_store_settings(MODULESTORE, doc_store_settings=DOC_STORE_CONFIG)
+DATA_DIR = "/openedx/data/"
+for store in MODULESTORE["default"]["OPTIONS"]["stores"]:
+   store["OPTIONS"]["fs_root"] = DATA_DIR
+
+# Behave like memcache when it comes to connection errors
+DJANGO_REDIS_IGNORE_EXCEPTIONS = True
+
+# Elasticsearch connection parameters
+ELASTIC_SEARCH_CONFIG = [{
+  
+  "host": "elasticsearch",
+  "port": 9200,
+}]
+
+CONTACT_MAILING_ADDRESS = "My Open edX - http://local.overhang.io"
+
+DEFAULT_FROM_EMAIL = ENV_TOKENS.get("DEFAULT_FROM_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+DEFAULT_FEEDBACK_EMAIL = ENV_TOKENS.get("DEFAULT_FEEDBACK_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+SERVER_EMAIL = ENV_TOKENS.get("SERVER_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+TECH_SUPPORT_EMAIL = ENV_TOKENS.get("TECH_SUPPORT_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+CONTACT_EMAIL = ENV_TOKENS.get("CONTACT_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+BUGS_EMAIL = ENV_TOKENS.get("BUGS_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+UNIVERSITY_EMAIL = ENV_TOKENS.get("UNIVERSITY_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+PRESS_EMAIL = ENV_TOKENS.get("PRESS_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+PAYMENT_SUPPORT_EMAIL = ENV_TOKENS.get("PAYMENT_SUPPORT_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+BULK_EMAIL_DEFAULT_FROM_EMAIL = ENV_TOKENS.get("BULK_EMAIL_DEFAULT_FROM_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+API_ACCESS_MANAGER_EMAIL = ENV_TOKENS.get("API_ACCESS_MANAGER_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+API_ACCESS_FROM_EMAIL = ENV_TOKENS.get("API_ACCESS_FROM_EMAIL", ENV_TOKENS["CONTACT_EMAIL"])
+
+# Get rid completely of coursewarehistoryextended, as we do not use the CSMH database
+INSTALLED_APPS.remove("lms.djangoapps.coursewarehistoryextended")
+DATABASE_ROUTERS.remove(
+    "openedx.core.lib.django_courseware_routers.StudentModuleHistoryExtendedRouter"
+)
+
+# Set uploaded media file path
+MEDIA_ROOT = "/openedx/media/"
+
+# Add your MFE and third-party app domains here
+CORS_ORIGIN_WHITELIST = []
+
+# Video settings
+VIDEO_IMAGE_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT
+VIDEO_TRANSCRIPTS_SETTINGS["STORAGE_KWARGS"]["location"] = MEDIA_ROOT
+
+GRADES_DOWNLOAD = {
+    "STORAGE_TYPE": "",
+    "STORAGE_KWARGS": {
+        "base_url": "/media/grades/",
+        "location": "/openedx/media/grades",
+    },
+}
+
+ORA2_FILEUPLOAD_BACKEND = "filesystem"
+ORA2_FILEUPLOAD_ROOT = "/openedx/data/ora2"
+ORA2_FILEUPLOAD_CACHE_NAME = "ora2-storage"
+
+# Change syslog-based loggers which don't work inside docker containers
+LOGGING["handlers"]["local"] = {
+    "class": "logging.handlers.WatchedFileHandler",
+    "filename": os.path.join(LOG_DIR, "all.log"),
+    "formatter": "standard",
+}
+LOGGING["handlers"]["tracking"] = {
+    "level": "DEBUG",
+    "class": "logging.handlers.WatchedFileHandler",
+    "filename": os.path.join(LOG_DIR, "tracking.log"),
+    "formatter": "standard",
+}
+LOGGING["loggers"]["tracking"]["handlers"] = ["console", "local", "tracking"]
+# Silence some loggers (note: we must attempt to get rid of these when upgrading from one release to the next)
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning, module="newrelic.console")
+warnings.filterwarnings("ignore", category=DeprecationWarning, module="lms.djangoapps.course_wiki.plugins.markdownedx.wiki_plugin")
+warnings.filterwarnings("ignore", category=DeprecationWarning, module="wiki.plugins.links.wiki_plugin")
+
+# Email
+EMAIL_USE_SSL = False
+# Forward all emails from edX's Automated Communication Engine (ACE) to django.
+ACE_ENABLED_CHANNELS = ["django_email"]
+ACE_CHANNEL_DEFAULT_EMAIL = "django_email"
+ACE_CHANNEL_TRANSACTIONAL_EMAIL = "django_email"
+EMAIL_FILE_PATH = "/tmp/openedx/emails"
+
+LOCALE_PATHS.append("/openedx/locale/contrib/locale")
+LOCALE_PATHS.append("/openedx/locale/user/locale")
+
+# Allow the platform to include itself in an iframe
+X_FRAME_OPTIONS = "SAMEORIGIN"
+
+
+JWT_AUTH["JWT_ISSUER"] = "http://local.overhang.io/oauth2"
+JWT_AUTH["JWT_AUDIENCE"] = "openedx"
+JWT_AUTH["JWT_SECRET_KEY"] = "6cOM7Nag34onNqtCSa5V4HWP"
+JWT_AUTH["JWT_PRIVATE_SIGNING_JWK"] = json.dumps(
+    {
+        "kid": "openedx",
+        "kty": "RSA",
+        "e": "AQAB",
+        "d": "PVW7rXF3_3S1H1fF_jsxCIkBFxkuiSziQxoMCs9tw7Q9EeFqxue0NS09DnJwTssUHAjaa-oPNLHP6dcrWbEykwS89_dS4W-2UCNk-X6eX4wykfEduOD88agfN9tv9eQqwxL_DlUn6qVPRUd9po4PcxJJsDJWM0mNt4ce4op-_rb45xL1IhPgG3BalIr6RQgbLf1j6TQgL7q1o3JhuECnuZqv4pfo7daVrRZHyoFMtxU7I20fZZGdmBijTRQ-ZNOLms5_5MvPsiY5ial6nuBdhxsyRdnvqb5CH-5sj-ys0QYR1G6bslIi-Sn6wA-o83lf1EeZ4LpRQBJR1PVWTamwUQ",
+        "n": "v4h4wBvHwQSbD9stogQ2J-mTURumAd8VNlJThKWn_iua7biZy_HW50SRZYrb_odcPixCj9ITba9UZNeT_Bpy1bm2_b8242DiyK2qKT-vLYrNg5nGxEygN31YbFnS9OneWTzhxv0vb4qhKuKmk6pxblozlERvjv6Vx9N7c71R5nB1hXgoWCE9q1cdXvm8l6OTCDcxi88vVCWdQQ9K-WANrGwOqTkCEFrieBWqhEgvnN6vJbCCo9hA-1mUZXY2IH65Jvoa5H2gul_t89Ot-T78uiUxV3qJ-6mePdkEj7my3v8jHJRoSmp7_plwQxOnbJ54TJJ0QVHkqXqKWy1VY6beBw",
+        "p": "2xJse8JtyWq09BrG6W-hWIJqYNt5uT3ejwsWLRIbuFpjRQ-2TDnUuhQHRBTLASEPSjV_cCJEYC04GyisM_fUo3ux8i8H14I7x9NEEWAVcjgVD6XGAX-QnNaWrnRrswXTCV7ykQS9MVJmAN_tote4Z8EMD3NRDSxupJpVeEa4LUk",
+        "q": "39Gq2xuMwnyeIreiRK7m8GTiAR6zfbmJh8AEl9beQq2_C2FUN52prBKS7uW_bScFtZuk264G8K2oIoyt9ZsxQgdsq97gQa___2-52ZvF13WsVb4YidrXOR8jP_BHg2IiE1fF-EsIrPwvF_nKBS199oTGMyFcutF_qG4v9GYoQM8",
+    }
+)
+JWT_AUTH["JWT_PUBLIC_SIGNING_JWK_SET"] = json.dumps(
+    {
+        "keys": [
+            {
+                "kid": "openedx",
+                "kty": "RSA",
+                "e": "AQAB",
+                "n": "v4h4wBvHwQSbD9stogQ2J-mTURumAd8VNlJThKWn_iua7biZy_HW50SRZYrb_odcPixCj9ITba9UZNeT_Bpy1bm2_b8242DiyK2qKT-vLYrNg5nGxEygN31YbFnS9OneWTzhxv0vb4qhKuKmk6pxblozlERvjv6Vx9N7c71R5nB1hXgoWCE9q1cdXvm8l6OTCDcxi88vVCWdQQ9K-WANrGwOqTkCEFrieBWqhEgvnN6vJbCCo9hA-1mUZXY2IH65Jvoa5H2gul_t89Ot-T78uiUxV3qJ-6mePdkEj7my3v8jHJRoSmp7_plwQxOnbJ54TJJ0QVHkqXqKWy1VY6beBw",
+            }
+        ]
+    }
+)
+JWT_AUTH["JWT_ISSUERS"] = [
+    {
+        "ISSUER": "http://local.overhang.io/oauth2",
+        "AUDIENCE": "openedx",
+        "SECRET_KEY": "6cOM7Nag34onNqtCSa5V4HWP"
+    }
+]
+
+# Disable codejail support
+# explicitely configuring python is necessary to prevent unsafe calls
+import codejail.jail_code
+codejail.jail_code.configure("python", "nonexistingpythonbinary", user=None)
+# another configuration entry is required to override prod/dev settings
+CODE_JAIL = {
+    "python_bin": "nonexistingpythonbinary",
+    "user": None,
+}
+
+# Custom features
+# LTI 1.3 will be enabled by default after lilac, and it's going to be a big
+# deal, so we enable it early. We should remove this once the feature flag is
+# deprecated.
+FEATURES["LTI_1P3_ENABLED"] = True
+
+
+######## End of settings common to LMS and CMS
+
+######## Common CMS settings
+
+STUDIO_NAME = u"My Open edX - Studio"
+MAX_ASSET_UPLOAD_FILE_SIZE_IN_MB = 100
+
+FRONTEND_LOGIN_URL = LMS_ROOT_URL + '/login'
+FRONTEND_LOGOUT_URL = LMS_ROOT_URL + '/logout'
+FRONTEND_REGISTER_URL = LMS_ROOT_URL + '/register'
+
+# Create folders if necessary
+for folder in [LOG_DIR, MEDIA_ROOT, STATIC_ROOT_BASE]:
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+
+
+
+######## End of common CMS settings
+
+ALLOWED_HOSTS = [
+    ENV_TOKENS.get("CMS_BASE"),
+    "cms",
+]
+
